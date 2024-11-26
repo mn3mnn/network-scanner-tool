4de66e2fee027b5b3cb7b85fdb48ad045eac7429
Monem <abdoeladawy4@gmail.com>
2024-11-26 15:02:52 +0200

    implement arp scan

--- network_tools/arp_scanner.py
+++ network_tools/arp_scanner.py
@@ -1,6 +1,7 @@
 import scapy.all as scapy
 from scapy.layers.inet import IP, TCP, UDP, ICMP
 from scapy.layers.l2 import Ether, ARP
+from scapy.sendrecv import srp
 
 
 class ARPScanner:
@@ -9,17 +10,27 @@ class ARPScanner:
         pass
 
     @staticmethod
-    def scan(subnet: str) -> dict[str, str]:
+    def scan(subnet: str, gui_callback: callable) -> dict[str, str]:
         """
         :param subnet:
         :return: A dictionary of IP addresses and corresponding MAC addresses.
         """
-        # request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet)
-        #
-        # ans, unans = srp(request, timeout=2, retry=1)
-        # result = []
-        #
-        # for sent, received in ans:
-        #     result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+        request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet)
 
-        return {}
+        ans, unans = srp(request, timeout=2, retry=1)
+        result = {}
+
+        for sent, received in ans:
+            result[str(received.psrc)] = str(received.hwsrc)
+
+        if gui_callback:
+            gui_callback(str(result))
+
+        return result
+
+
+if __name__ == '__main__':
+    scanner = ARPScanner()
+    result = scanner.scan("192.168.1.0/24")
+    print(result)
+
